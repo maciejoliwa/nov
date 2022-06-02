477935e016fd562f60f0144fd135ca0680fd214c
maciejoliwa <maciejoliwa0906@gmail.com>
2022-06-02 19:15:50 +0200

    added a few cli options

--- src/__main__.py
+++ src/__main__.py
@@ -9,9 +9,21 @@ import subprocess
 
 
 if __name__ == '__main__':
-    argparser = argparse.ArgumentParser(description="nov transpiler")
+    argparser = argparse.ArgumentParser(prog='nov', description="nov transpiler")
     argparser.add_argument('Novfile',
                             metavar='novfile',
                             type=str,
                             help='Path to the nov file'
     )
+    argparser.add_argument('Outfile',
+                            metavar='outfile',
+                            type=str,
+                            help='Path to the output file'
+    )
+    argparser.add_argument('-n', '--no-run',
+                            action='store_true',
+                            help='Your .js file won\'t be run after transpilation'
+    )
+
+    args = argparser.parse_args()
+    print(args)
